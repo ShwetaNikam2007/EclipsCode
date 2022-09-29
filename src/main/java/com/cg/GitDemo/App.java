cb51fd263577d48b658b63b4de2e4adf662a5b22
shwetnik <shwetanikam60@gmail.com>
2022-09-29 17:01:43 +0530

    Eclipse project of addition

---
+++ src/main/java/com/cg/GitDemo/App.java
@@ -0,0 +1,16 @@
+package com.cg.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int a = 10;
+    	int b = 20;
+    	int result = a + b;
+        System.out.println( "Addition is : "+result );
+    }
+}
